e5dc4f1507bf59db6739e11d7019ba25fbcb6d53
th_heartless@mail.ru <th_heartless@mail.ru>
2021-09-23 23:35:03 +0300

    add comments

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -16,12 +16,14 @@ namespace dz3
             decimal flatsInPadik = floors * flatsByfloor;
 
             Console.WriteLine($"flats: {flats}; flats in padik: {flatsInPadik}");
+
             //  7 8 9   16 17 18   25 26 27   34 35 36
             //  4 5 6   13 14 15   22 23 24   31 32 33
             //  1 2 3   10 11 12   19 20 21   28 29 30
             //  w w w
             //  r r r
             //  w w w
+
             for (decimal flat = 1; flat <= flats; flat++)
             {
                 string color = "W";
